b8268259e6d42fc41e9c33cc2605c560c43a482a
asar <bartosz@skorupa.net>
2019-04-23 20:22:36 +0200

    some more initial

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () 
 {
     let win = new BrowserWindow({ width: 800, height: 600 })
-    win.loadFile('index.html')
-    win.webContents.openDevTools()
+    win.loadFile('scripts/index.html')
+
+    devtools = new BrowserWindow()
+    win.webContents.setDevToolsWebContents(devtools.webContents)
+    win.webContents.openDevTools({mode: 'detach'})
+    win.webContents.once('did-finish-load', function () {
+        let winBounds = win.getBounds()
+        devtools.setPosition(500,200)
+    })
 
     win.on('closed', () => 
     {
@@ -13,7 +20,7 @@ function createWindow ()
 }
 
 app.on('ready', createWindow)
-app.on('window-all-closed', () => 
+app.on('window-all-closed', () =>
 {
     if (process.platform !== 'darwin')
     {
